e20acd7eae2a44ad3c4e2a131aba751ca31c60fe
aleixgr <aleix1gr@gmail.com>
2017-06-14 15:57:57 +0200

    Add files via upload

---
+++ media/smiteguide/java.js
@@ -0,0 +1,170 @@
+
+/*******************************************************************************************MEDUSA****/
+function medusa() {
+    localStorage.setItem("nomDeu", "Medusa");
+    localStorage.setItem("godImg", "img/infomedusa.png");
+    localStorage.setItem("godAudio", "audio/Medusa_Select.ogg");
+    localStorage.setItem("godVideo", "video/godrevealmedusa.mp4");
+    localStorage.setItem("poster", "img/medusa_poster.jpg");
+    localStorage.setItem("ab1_title1", "Sidewinder");
+    localStorage.setItem("ab1_title2", "Viper Shot");
+    localStorage.setItem("ab1_title3", "Acid Spray");
+    localStorage.setItem("ab1_title4", "Lacerate");
+    localStorage.setItem("ab1_title5", "Petrify");
+    localStorage.setItem("ab1_contxt1", "Medusa suffers no movement penalty when side strafing and half of the movement penalty when moving backward.");
+    localStorage.setItem("ab1_contxt2", "Medusa shoots out 3 vipers in rapid succession that poison the targets hit dealing damage over time.");
+    localStorage.setItem("ab1_contxt3", "Medusa hurls back and spits out acid that damages the first enemy hit and sprays out acid in a cone behind them. Acid Spray will explode on statues Medusa has created.");
+    localStorage.setItem("ab1_contxt4", "Medusa quickly slithers forward rooting and slashing the first enemy god she encounters. The slash leaves the enemy god reeling in pain, reducing their healing for a short time.");
+    localStorage.setItem("ab1_contxt5", "Medusa removes her mask revealing her true face, delivering a powerful blast from her horrifying Gorgon gaze. Enemies take damage and are stunned if they are looking at Medusa. Enemies not looking at Medusa take 75% damage and are slowed. Enemy gods that are killed during Medusa's gaze will turn to a stone statue and remain behind as a reminder to others.");
+    localStorage.setItem("ab1_type1", "PASSIVE");
+    localStorage.setItem("ab1_type2", "BUFF");
+    localStorage.setItem("ab1_type3", "LINE");
+    localStorage.setItem("ab1_type4", "DASH");
+    localStorage.setItem("ab1_type5", "CONE");
+    location.href = "detalls.html";
+}
+
+/*******************************************************************************************AH PUCH****/
+
+function ahpuch() {
+    localStorage.setItem("nomDeu", "Ah Puch");
+    localStorage.setItem("godImg", "img/infoahpuch.png");
+    localStorage.setItem("godAudio", "audio/Ah_Puch_Select.ogg");
+    localStorage.setItem("godVideo", "video/godrevealahpuch.mp4");
+    localStorage.setItem("poster", "img/ahpuch_poster.jpg");
+    localStorage.setItem("ab1_title1", "Hollow Ground");
+    localStorage.setItem("ab1_title2", "Undead Surge");
+    localStorage.setItem("ab1_title3", "Corpse Explosion");
+    localStorage.setItem("ab1_title4", "Fleeting Breath");
+    localStorage.setItem("ab1_title5", "Empty the Crypts");
+    localStorage.setItem("ab1_contxt1", "Ah Puch is closely attuned with the dead and may exhume corpses at his will. When walking over a decaying corpse, Ah Puch lowers the cooldown of Undead Surge and Corpse Explosion by 2s, restores 5% of his maximum Health and 3% of his maximum Mana.");
+    localStorage.setItem("ab1_contxt2", "Ah Puch raises up to two decaying corpses from the underworld that surge forward exploding on anything they touch, when they reach max distance, or when Ah Puch reactivates Undead Surge dealing damage and slowing targets hit. The corpses collapse after exploding remaining behind.");
+    localStorage.setItem("ab1_contxt3", "Ah Puch causes the target area to explode dealing damage. Corpse Explosion also causes any decaying corpses in the area to explode.");
+    localStorage.setItem("ab1_contxt4", "Ah Puch throws a charm from the underworld which awakens a decaying corpse that applies miasma to targets in an area and falls to the ground, remaining behind. Targets take damage every second. After the duration, targets take additional damage and are stunned if they were healed by an ability during the initial duration.");
+    localStorage.setItem("ab1_contxt5", "Ah Puch unleashes an army of Wraiths from the Ninth Hell that swarm forward and deal damage to enemies they collide with.");
+    localStorage.setItem("ab1_type1", "PASSIVE");
+    localStorage.setItem("ab1_type2", "PROJECTILE");
+    localStorage.setItem("ab1_type3", "GROUND TARGET");
+    localStorage.setItem("ab1_type4", "GROUND TARGET");
+    localStorage.setItem("ab1_type5", "LINE");
+    location.href = "detalls.html";
+}
+
+/*******************************************************************************************THANATOS****/
+
+function thanatos() {
+    localStorage.setItem("nomDeu", "Thanatos");
+    localStorage.setItem("godImg", "img/infothanatos.png");
+    localStorage.setItem("godAudio", "audio/Thanatos_Select.ogg");
+    localStorage.setItem("godVideo", "video/godrevealthanatos.mp4");
+    localStorage.setItem("poster", "img/thanatos_poster.jpg");
+    localStorage.setItem("ab1_title1", "Harvester of Souls");
+    localStorage.setItem("ab1_title2", "Death Scythe");
+    localStorage.setItem("ab1_title3", "Scent of Death");
+    localStorage.setItem("ab1_title4", "Soul Reap");
+    localStorage.setItem("ab1_title5", "Hovering Death");
+    localStorage.setItem("ab1_contxt1", "Enemy gods within his ultimate's execute Health threshold are revealed to Thanatos. Dealing the killing blow to enemies also empowers Thanatos, healing him based on the max HP of his target and reducing his active cooldowns. Additionally, all of Thanatos' abilities cost Health and Mana to use.");
+    localStorage.setItem("ab1_contxt2", "Thanatos flings his scythe at an enemy, dealing damage and an additional 10% of the target's maximum Health against enemy gods. Enemies hit are slowed for 3s and heal Thanatos based on damage dealt. Consumes 5% of Thanatos' current HP when used.");
+    localStorage.setItem("ab1_contxt3", "Thanatos flings his scythe at an enemy, dealing damage and an additional 10% of the target's maximum Health against enemy gods. Enemies hit are slowed for 3s and heal Thanatos based on damage dealt. Consumes 5% of Thanatos' current HP when used.");
+    localStorage.setItem("ab1_contxt4", "Thanatos swings his scythe, dealing Physical damage and Silencing enemies he hits. Thanatos moves at a reduced rate while swinging. Consumes 5% of Thanatos' current HP when used.");
+    localStorage.setItem("ab1_contxt5", "Thanatos flaps his wings and ascends for 5s, unable to be hit. While airborne, he moves unimpeded at an increased speed before diving to his target location, dealing damage. Enemies below a Health threshold are instantly killed. All others are Stunned. Consumes 10% of Thanatos' current HP when used.");
+    localStorage.setItem("ab1_type1", "PASSIVE");
+    localStorage.setItem("ab1_type2", "PROJECTILE");
+    localStorage.setItem("ab1_type3", "BUFF");
+    localStorage.setItem("ab1_type4", "CONE");
+    localStorage.setItem("ab1_type5", "GROUND TARGET");
+    location.href = "detalls.html";
+}
+
+/*******************************************************************************************THANATOS****/
+
+function terra() {
+    localStorage.setItem("nomDeu", "Terra");
+    localStorage.setItem("godImg", "img/infoterra.png");
+    localStorage.setItem("godAudio", "audio/Terra_Select.ogg");
+    localStorage.setItem("godVideo", "video/godrevealterra.mp4");
+    localStorage.setItem("poster", "img/terra_poster.jpg");
+    localStorage.setItem("ab1_title1", "Standing Stones");
+    localStorage.setItem("ab1_title2", "Force of Nature");
+    localStorage.setItem("ab1_title3", "Crushing Earth");
+    localStorage.setItem("ab1_title4", "Monolith");
+    localStorage.setItem("ab1_title5", "Terra's Blessing");
+    localStorage.setItem("ab1_contxt1", "Terra's abilities create Standing Stones. As long as any Standing Stones are on the battleground Terra becomes Knockup immune and her Basic Attacks will Slow enemies and deal bonus damage. If Terra Shatters any stones she will retain this buff for an additional 3s after all of the stones are gone.");
+    localStorage.setItem("ab1_contxt2", "Terra gains momentum, causing her to dash forward and deal damage to enemies she passes through. Terra may charge through her own Standing Stones to Shatter them. If she does she may charge one more time within the next 3s");
+    localStorage.setItem("ab1_contxt3", "Terra raises up two Standing Stone walls on either side of a target location. As long as both stones stand, Terra may activate this ability again to slam the stones together, damaging and Stunning enemies in between for 1.5s. These stones can be Shattered into a cone of shrapnel with Force of Nature, damaging enemies in the area.");
+    localStorage.setItem("ab1_contxt4", "Terra summons a Standing Stone monolith surging with natural energy. The monolith heals nearby allied gods every .5s for the duration but can be detroyed early if hit by 3 enemy basic attacks. Terra can Shatter this stone with Force of Nature to Root enemies in the area for 1s and create a damaging area that lasts 5s.");
+    localStorage.setItem("ab1_contxt5", "Terra channels her strength into the entire world, buffing all of her allies for 10s. While active, Terra and her allies gain increased Movement Speed, reduction of Mana costs for abilities, and increased Cooldown Reduction. Terra also summons 4 stone around herself and each of her teammates. If any one of them take damage 4 times the stones will activate and heal that god.");
+    localStorage.setItem("ab1_type1", "PASSIVE");
+    localStorage.setItem("ab1_type2", "DASH");
+    localStorage.setItem("ab1_type3", "GROUND TARGET");
+    localStorage.setItem("ab1_type4", "GROUND TARGET");
+    localStorage.setItem("ab1_type5", "GLOBAL BUFF");
+    location.href = "detalls.html";
+}
+
+/*******************************************************************************************FUNCIO CARGAR****/
+
+window.onload=function(){
+    document.getElementById("medusa_txt").innerHTML = localStorage.getItem("nomDeu");
+    document.getElementById("medusa-wall").src=localStorage.getItem("godImg");
+    document.getElementById("godaudio").src=localStorage.getItem("godAudio");
+    document.getElementById("bunny").setAttribute("src", localStorage.getItem("godVideo"));
+    document.getElementById("bunny").setAttribute("poster", localStorage.getItem("poster"));
+    document.getElementById("text_title1").innerHTML = localStorage.getItem("ab1_title1");
+    document.getElementById("text_title2").innerHTML = localStorage.getItem("ab1_title2");
+    document.getElementById("text_title3").innerHTML = localStorage.getItem("ab1_title3");
+    document.getElementById("text_title4").innerHTML = localStorage.getItem("ab1_title4");
+    document.getElementById("text_title5").innerHTML = localStorage.getItem("ab1_title5");
+    document.getElementById("text_normal1").innerHTML = localStorage.getItem("ab1_contxt1");
+    document.getElementById("text_normal2").innerHTML = localStorage.getItem("ab1_contxt2");
+    document.getElementById("text_normal3").innerHTML = localStorage.getItem("ab1_contxt3");
+    document.getElementById("text_normal4").innerHTML = localStorage.getItem("ab1_contxt4");
+    document.getElementById("text_normal5").innerHTML = localStorage.getItem("ab1_contxt5");
+    document.getElementById("text_normal6").innerHTML = localStorage.getItem("ab1_type1");
+    document.getElementById("text_normal7").innerHTML = localStorage.getItem("ab1_type2");
+    document.getElementById("text_normal8").innerHTML = localStorage.getItem("ab1_type3");
+    document.getElementById("text_normal9").innerHTML = localStorage.getItem("ab1_type4");
+    document.getElementById("text_normal10").innerHTML = localStorage.getItem("ab1_type5");
+    
+    
+var videoBunny = document.getElementById("bunny");
+var bReproduir = document.getElementById("botoReproduir"); 
+var bPausar = document.getElementById("botoPausar");
+var dTotal = document.getElementById("duraciototal"); 
+var pos = document.getElementById("posicioactual");
+var bMes = document.getElementById("mes");
+var bMenys = document.getElementById("menys");
+
+bReproduir.onclick = function(){
+    videoBunny.play(); 
+    videoBunny.addEventListener('timeupdate', updateProgressBar, false);
+    dTotal.innerHTML = Math.floor(videoBunny.duration);
+    function updateProgressBar() {
+    var progressBar = document.getElementById('progress-bar');
+    var percentage = Math.floor((100 / videoBunny.duration) *
+    videoBunny.currentTime);
+    progressBar.value = percentage;
+    progressBar.innerHTML = percentage + '% played';
+    }
+    
+}
+
+bPausar.onclick = function(){
+    videoBunny.pause();  
+}
+
+bMes.onclick = function(){
+    videoBunny.volume = videoBunny.volume + 0.2; 
+}
+
+bMenys.onclick = function(){
+    videoBunny.volume = videoBunny.volume - 0.2; 
+}
+
+videoBunny.ontimeupdate = function(){
+   pos.innerHTML = Math.floor(videoBunny.currentTime); 
+}
+
+
+    
+}
