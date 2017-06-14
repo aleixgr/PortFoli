4cfb98f5f6616428e0c20e915345ee0c658b5e97
aleixgr <aleix1gr@gmail.com>
2017-06-14 15:54:39 +0200

    Add files via upload

---
+++ media/historiainteractiva/java.js
@@ -0,0 +1,126 @@
+function bleft_vall(){
+    document.getElementById('petita_introduccio').style.display = 'none';
+    document.getElementById('vall').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/vall.jpg')";
+}
+
+function bright_bosc(){
+    document.getElementById('petita_introduccio').style.display = 'none';
+    document.getElementById('bosc').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/bosc.jpg')";
+}
+
+function bleft_cova(){
+    document.getElementById('bosc').style.display = 'none';
+    document.getElementById('cova').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/cova.jpg')";
+}
+
+function bright_tornar(){
+    document.getElementById('cova').style.display = 'none';
+    document.getElementById('bosc').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/bosc.jpg')";
+}
+
+function bleft_seguir(){
+    document.getElementById('cova').style.display = 'none';
+    document.getElementById('seguir').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/cova_skelletons.jpg')";
+}
+
+
+function bleft_inici2(){
+    document.getElementById('entrar').style.display = 'none';
+    document.getElementById('inici2').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/Escena1.png')";
+}
+
+function bright_explorar(){
+    document.getElementById('bosc').style.display = 'none';
+    document.getElementById('explorar').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/castell.jpg')";
+}
+
+function bleft_tornar2(){
+    document.getElementById('explorar').style.display = 'none';
+    document.getElementById('bosc').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/bosc.jpg')";
+}
+
+function bright_entrar(){
+    document.getElementById('explorar').style.display = 'none';
+    document.getElementById('entrar').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/dintrecastell.jpg')";
+}
+
+function bleft_vall2(){
+    document.getElementById('inici2').style.display = 'none';
+    document.getElementById('vall').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/vall.jpg')";
+}
+
+function bright_bosc2(){
+    document.getElementById('inici2').style.display = 'none';
+    document.getElementById('bosc').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/bosc.jpg')";
+}
+
+function bright_saberdeath(){
+    document.getElementById('entrar').style.display = 'none';
+    document.getElementById('saberdeath').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/saberespasa.jpg')";
+}
+
+function bright_casa(){
+    document.getElementById('vall').style.display = 'none';
+    document.getElementById('casa').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/casabuida.jpg')";
+}
+
+function bleft_retorn3(){
+    document.getElementById('casa').style.display = 'none';
+    document.getElementById('vall').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/vall.jpg')";
+}
+
+function bleft_riu(){
+    document.getElementById('vall').style.display = 'none';
+    document.getElementById('riu').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/river.jpg')";
+}
+
+function bleft_retorn4(){
+    document.getElementById('riu').style.display = 'none';
+    document.getElementById('vall').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/vall.jpg')";
+}
+
+function bright_temple(){
+    document.getElementById('riu').style.display = 'none';
+    document.getElementById('temple').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/illamedusa.jpg')";
+}
+
+function bleft_serp(){
+    document.getElementById('temple').style.display = 'none';
+    document.getElementById('altar').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/altar.jpg')";
+}
+
+function bleft_seguent(){
+    document.getElementById('altar').style.display = 'none';
+    document.getElementById('altar2').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/altar.jpg')";
+}
+
+function bleft_final(){
+    document.getElementById('altar2').style.display = 'none';
+    document.getElementById('final').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/final.jpg')";
+}
+
+function bright_centpeus(){
+    document.getElementById('temple').style.display = 'none';
+    document.getElementById('medusadeath').style.display = 'block';
+    document.getElementById('display').style.backgroundImage = "url('img/fondomedusa.jpg')";
+}
